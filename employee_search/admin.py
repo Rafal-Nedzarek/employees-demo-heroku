da6563b434e6d4fd937a7270a757d71a6c77e958
Rafal-Nedzarek <37933366+Rafal-Nedzarek@users.noreply.github.com>
2019-05-21 22:24:55 +0100

    added RedirectView in urls and customized admin

--- employee_search/admin.py
+++ employee_search/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from employee_search.models import Titles, Departments, Employees
 # Register your models here.
 admin.site.register(Titles)
-admin.site.register(Departments)
-admin.site.register(Employees)
+
+
+@admin.register(Employees)
+class EmployeesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'salary', 'title',
+                    'department', 'manager')
+    list_filter = ('title', 'department')
+
+
+# the following two classes are added just for training
+# getting the inlines can be pretty slow...
+class EmployeesInline(admin.TabularInline):
+    model = Employees
+    extra = 0
+
+
+@admin.register(Departments)
+class DepartmentsAdmin(admin.ModelAdmin):
+    inlines = [EmployeesInline]
